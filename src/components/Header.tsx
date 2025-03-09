feed89e27fb8b7ea319105e68e8d5603edb96b41
Nikhil Ranjan <niklabh811@gmail.com>
2025-03-09 11:53:24 +0400

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import ThemeToggle from './ThemeToggle';
 const Header = () => {
   return (
     <div className="border-b border-gray-200 dark:border-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <div className="max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-8">
             <Logo />
